ed05920ec26bbe132f5fa097c147843c348dbf53
viac92 <viac92@gmail.com>
2022-11-28 09:38:43 +0000

    add migration for the BNBTest

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Cryptle = artifacts.require("./Cryptle.sol");
+const BNBTest = artifacts.require("./BNBTest.sol");
 
 module.exports = async function(deployer) {
     deployer.deploy(Cryptle);
+    deployer.deploy(BNBTest, Cryptle.address);
 }
